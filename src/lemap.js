51b96ab2c4cca24a8c1db40cfc95a4c821b7cb9f
carban <murillo.carlos@correounivalle.edu.co>
2019-11-18 14:52:09 -0500

    new features on map

--- src/lemap.js
+++ src/lemap.js
@@ -1,28 +1,55 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
 import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
+import L from 'leaflet';
 
-class Mapi extends Component{
-    constructor(){
-        super();
-        this.state = {position: [51.505, -0.09], position2: [51.505, -0.11]};
-    }
-    render(){
-        return(
-            <Map className="amapa" center={this.state.position} zoom={13}>
-            <TileLayer
-              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-              attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
-            />
-            <Marker position={this.state.position}>
-              <Popup>Custom Message!!.<br />Easily customizable.</Popup>
-            </Marker>
-            <Marker position={this.state.position2}>
-              <Popup>Grimes.<br />Easily customizable.</Popup>
-            </Marker>
-          </Map>
-        )
-    }
+class Mapi extends Component {
+  constructor() {
+    super();
+    this.state = {
+      position: [3.4516, -76.5320],
+      movingMarker: [3.4616, -76.5000],
+      pos: []
+    };
+  }
+
+  action = e => {
+    this.setState({ movingMarker: [e.latlng.lat, e.latlng.lng] });
+  }
+
+  newPoint = () => {
+    this.setState({ pos: [...this.state.pos, this.state.movingMarker] });
+  }
+
+  render() {
+
+    const myicon = new L.Icon({
+      iconUrl: require('./flag.png'),
+      shadowUrl: '',
+      iconAnchor: [10, 50],
+      popupAnchor: [10, -50],
+      iconSize: new L.Point(40, 50),
+    });
+    
+    const allMarkers = this.state.pos.map((ele, i) => (
+      <Marker position={ele} key={i}>
+        <Popup>Marker: {i}<br></br>Lat: {ele[0]}<br></br>Lng: {ele[1]}</Popup>
+      </Marker>)
+    );
+
+    return (
+      <Map className="amapa" center={this.state.position} zoom={13} onClick={this.action}>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+        />
+        <Marker position={this.state.movingMarker} icon={myicon}>
+          <Popup><button onClick={this.newPoint}>New Point</button></Popup>
+        </Marker>
+        {allMarkers}
+      </Map>
+    )
+  }
 }
 
 export default Mapi;
