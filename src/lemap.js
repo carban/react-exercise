972130d3d7a6f6434c06805d4d735b9a880120db
carban <murillo.carlos@correounivalle.edu.co>
2019-11-18 15:53:07 -0500

    layers on map

--- src/lemap.js
+++ src/lemap.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
-import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
+import { Map, Marker, Popup, TileLayer, LayersControl, FeatureGroup, Circle } from 'react-leaflet';
 import L from 'leaflet';
 
 class Mapi extends Component {
@@ -30,7 +30,7 @@ class Mapi extends Component {
       popupAnchor: [10, -50],
       iconSize: new L.Point(40, 50),
     });
-    
+
     const allMarkers = this.state.pos.map((ele, i) => (
       <Marker position={ele} key={i}>
         <Popup>Marker: {i}<br></br>Lat: {ele[0]}<br></br>Lng: {ele[1]}</Popup>
@@ -39,6 +39,35 @@ class Mapi extends Component {
 
     return (
       <Map className="amapa" center={this.state.position} zoom={13} onClick={this.action}>
+        <LayersControl position="topright">
+          <LayersControl.BaseLayer name="OpenStreetMap.BlackAndWhite">
+            <TileLayer
+              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png"
+            />
+          </LayersControl.BaseLayer>
+          <LayersControl.BaseLayer name="OpenStreetMap.Mapnik">
+            <TileLayer
+              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+          </LayersControl.BaseLayer>
+          <LayersControl.Overlay name="Marker with popup">
+            <Marker position={[3.4516, -76.5320]}>
+              <Popup>
+                <span>A pretty CSS3 popup. <br /> Easily customizable.</span>
+              </Popup>
+            </Marker>
+          </LayersControl.Overlay>
+          <LayersControl.Overlay name="Feature group">
+            <FeatureGroup color="purple">
+              <Popup>
+                <span>Popup in FeatureGroup</span>
+              </Popup>
+              <Circle center={[3.4316, -76.5520]} radius={200} />
+            </FeatureGroup>
+          </LayersControl.Overlay>
+        </LayersControl>
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
